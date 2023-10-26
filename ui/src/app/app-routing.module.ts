6df3b3fae034eeece493306f53359e3e9dd5c99e
Medelouali <Mohammedelouali1999@gmail.com>
2023-10-26 17:22:17 +0100

    Initial Commit

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { 
+    path: 'home', 
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule) 
+  }, 
+  { 
+    path: 'login', 
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule) 
+  }, 
+  { 
+    path: 'register', 
+    loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) 
+  },
+  { path: '**', redirectTo: '/home' } // Just for demo purposes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ ui/src/app/home/messaging/messaging.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MessagingRoutingModule } from './messaging-routing.module';
+import { MessagingComponent } from './messaging.component';
+
+
+@NgModule({
+  declarations: [
+    MessagingComponent
+  ],
+  imports: [
+    CommonModule,
+    MessagingRoutingModule
+  ]
+})
+export class MessagingModule { }
