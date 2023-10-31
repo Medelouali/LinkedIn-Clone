3fd1f909009d41a648cf46748f7850618fb08ac6
Medelouali <Mohammedelouali1999@gmail.com>
2023-10-31 14:37:18 +0100

    Adding some addons

---
+++ linkediners/src/main/java/com/ensa/linkediners/LinkedinersApplication.java
@@ -0,0 +1,13 @@
+package com.ensa.linkediners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinersApplication.class, args);
+	}
+
+}
