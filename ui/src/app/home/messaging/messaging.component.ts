351747d58bbb1bf11ad516890a56df092ec9bf01
Medelouali <Mohammedelouali1999@gmail.com>
2023-11-15 07:03:54 +0100

    Adding messages ui

--- ui/src/app/home/messaging/messaging.component.ts
+++ ui/src/app/home/messaging/messaging.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-messaging',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./messaging.component.css']
 })
 export class MessagingComponent {
+  currentTab=signal("FOCUSED");
+  isInputExtended=signal(false);
 
+  setCurrentTab(tab: "FOCUSED" | "OTHER"){
+    this.currentTab.set(tab);
+  }
+
+  toggleIsInputExtended(){
+    this.isInputExtended.update(v=>!v);
+  }
 }
